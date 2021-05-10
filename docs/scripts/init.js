382879c962fd929a62852825f4c231c55af860dc
András Németh <sunarch@protonmail.com>
2021-05-10 23:39:49 +0200

    Put GUI interaction function into object

--- docs/scripts/init.js
+++ docs/scripts/init.js
@@ -4,6 +4,8 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-cc = new OberstufenPunkteKalkulator();
-
 i18n = new Localization();
+
+gui = new GuiInteraction();
+
+cc = new OberstufenPunkteKalkulator();

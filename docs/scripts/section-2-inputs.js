3abe3537030d70d4f048df9b93ee3b2555c2c06d
András Németh <sunarch@protonmail.com>
2021-05-10 22:12:01 +0200

    Move GUI interaction function to separate script

--- docs/scripts/section-2-inputs.js
+++ docs/scripts/section-2-inputs.js
@@ -149,19 +149,3 @@ function inputs_setup() {
 
 }
 
-
-function updateGrade(s_fach, i_semester) {
-    s_id = "input_" + s_fach + "_note_" + i_semester
-    dom_input = document.getElementById(s_id)
-    s_value = dom_input.value
-
-    if (!valid.grade(s_value)) {
-        dom_input.value = "";
-        return;
-    }
-
-    i_fach = cc.fachIdConvert.indexOf(s_fach)
-    tempNum = new Number(s_value);
-    cc.fach[i_fach].note[i_semester] = tempNum.valueOf();
-
-}

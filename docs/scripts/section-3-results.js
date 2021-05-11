0de9a8530c5e92e3c8a5edad6d244729bf28dd2d
András Németh <sunarch@protonmail.com>
2021-05-11 09:45:40 +0200

    Move results setup into GUI object

--- docs/scripts/section-3-results.js
+++ docs/scripts/section-3-results.js
@@ -4,41 +4,4 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-function results_setup() {
 
-    document.getElementById('results_main_sum').innerHTML = cc.ergebnis_gesamt;
-    document.getElementById('results_main_avg').innerHTML = cc.ergebnis_durchschnitt;
-
-    document.getElementById('results_a_sum').innerHTML = cc.bereichA.ergebnis;
-    document.getElementById('results_b_sum').innerHTML = cc.bereichB.ergebnis;
-    document.getElementById('results_c_sum').innerHTML = cc.bereichC.ergebnis;
-
-    var dom_tbody_B = document.getElementById('results_b_tbody');
-
-    for(n1 = 1; n1 < cc.bereichB.all.length; n1 = n1 + 1) {
-
-        var dom_tr_B = document.createElement('tr');
-
-        // cell 1 - subject
-        var dom_td_B = document.createElement('td');
-        var dom_text = document.createTextNode(cc.bereichB.all[n1].fachname);
-        dom_td_B.appendChild(dom_text);
-        dom_tr_B.appendChild(dom_td_B);
-
-        // cell 1 - semester
-        var dom_td_B = document.createElement('td');
-        var dom_text = document.createTextNode("  " + cc.bereichB.all[n1].getName() + "  ");
-        dom_td_B.appendChild(dom_text);
-        dom_tr_B.appendChild(dom_td_B);
-
-        // cell 3 - grade
-        var dom_td_B = document.createElement('td');
-        var dom_text = document.createTextNode(cc.bereichB.all[n1].note + ' Punkte')
-        dom_td_B.appendChild(dom_text);
-        dom_tr_B.appendChild(dom_td_B);
-
-        // append
-        dom_tbody_B.appendChild(dom_tr_B);
-    }
-
-}

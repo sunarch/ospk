d440699ff8f8a4902a14c316aab04f8f91bb7526
András Németh <sunarch@protonmail.com>
2021-05-11 09:32:03 +0200

    Move inputs setup into GUI objects

--- docs/scripts/gui-3-grades.js
+++ docs/scripts/gui-3-grades.js
@@ -7,8 +7,11 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 function GuiInteractionsForGrades() {
 
     this.show = function() {
+
         if(cc.optsready.indexOf(false) == -1) {
-            inputs_setup();
+            this.setup();
+            gui.exams.setup();
+            gui.buttons.setup();
             document.getElementById('grades').style.display = "block";
             document.getElementById('exams').style.display = "block";
             document.getElementById('buttons').style.display = "block";
@@ -16,6 +19,7 @@ function GuiInteractionsForGrades() {
     }
 
     this.updateGrade = function(s_fach, i_semester) {
+
         s_id = "grades_" + s_fach + "_sem_" + i_semester
         dom_input = document.getElementById(s_id)
         s_value = dom_input.value
@@ -29,5 +33,23 @@ function GuiInteractionsForGrades() {
         tempNum = new Number(s_value);
         cc.fach[i_fach].note[i_semester] = tempNum.valueOf();
 
+    }
+
+    this.setup = function() {
+
+        document.getElementById('grades_title').innerHTML = i18n.get("grades1112_title");
+
+        document.getElementById('grades_header').innerHTML = i18n.get("grades1112_header");
+
+        for(n1 = 1; n1 < cc.fach.length; n1 = n1 + 1) {
+
+            document.getElementById("grades_" + cc.fach[n1].name).style.display = "none";
+
+            if(cc.fach[n1].active == true) {
+                document.getElementById("grades_" + cc.fach[n1].name).style.display = "table-row";
+            }
+
+            document.getElementById("grades_" + cc.fach[n1].name + "_subj").innerHTML = cc.fach[n1].getFullName();
+        }
     }
 }
--- docs/scripts/gui-5-buttons.js
+++ docs/scripts/gui-5-buttons.js
@@ -7,10 +7,19 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 function GuiInteractionsForButtons() {
 
     this.calculate = function() {
+
         cc.calculate();
     }
 
     this.exportData = function() {
+
         cc.exportData()
+    }
+
+    this.setup = function() {
+
+        document.getElementById('button_calculate').value = i18n.get("calculate");
+
+        document.getElementById('button_export').value = i18n.get("exportData");
     }
 }
